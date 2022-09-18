978a754c8dc2936d7da390fc4410354f74558021
Qasim-Elhabrush <qasimelhabrush@gmail.com>
2022-09-18 18:30:56 -0400

    First iteration of game complete

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import { Drawer } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { games } from "./games";
 import CharacterCard from "./Card";
-import Scores from './Scores'
+import Scores from "./Scores";
 
 export default function App() {
+  const [footerVisible,setFooterVisible] = React.useState(true)
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
   const [selectedGame, setSelectedGame] = React.useState(games[0]);
   const [currentScore, setCurrentScore] = React.useState(0);
   const [highScore, setHighScore] = React.useState([0, 0, 0, 0, 0]);
   const [clickedCharacters, setClickedCharacters] = React.useState([]);
   const [indexOfSelectedGame, setIndexOfSelectedGame] = React.useState(0);
-
+  const [characterArr,setCharacterArr] = React.useState([...selectedGame.characters]);
+  function shuffleCharacterArr(){
+    setCharacterArr(prevArr=>{
+      const newArr = [...prevArr];
+      shuffle(newArr);
+      return newArr
+    })
+  }
+  console.log(characterArr);
+  function shuffle(array) {
+    for (let i = array.length - 1; i >= 1; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // 0 <= j <= i
+      let temp = array[j];
+      array[j] = array[i];
+      array[i] = temp;
+    }
+  }
   function openDrawer() {
     if (isDrawerOpen !== true) {
       setIsDrawerOpen(true);
@@ -30,15 +47,19 @@ export default function App() {
   function restartGameProgress() {
     setClickedCharacters([]);
     setCurrentScore(0);
+    
   }
 
   function changeGame(indexOfGame) {
     restartGameProgress();
+    closeDrawer();
     setSelectedGame(games[indexOfGame]);
+    setCharacterArr([...games[indexOfGame].characters])
     setIndexOfSelectedGame(indexOfGame);
   }
 
-  function onClickCharacterHandler(characterName) { 
+  function onClickCharacterHandler(characterName) {
+    shuffleCharacterArr();
     if (clickedCharacters.includes(characterName)) {
       restartGameProgress();
     } else {
@@ -48,17 +69,17 @@ export default function App() {
         ...prevClickedCharacters,
         characterName,
       ]);
-      if (currentScore === 8) {
-        console.log('You win');
-      }
       if (increasedScore > highScore[indexOfSelectedGame]) {
         setHighScore((prevHighScore) => {
           const newArr = [...prevHighScore];
-          newArr[indexOfSelectedGame] = currentScore;
+          newArr[indexOfSelectedGame] = increasedScore;
           return newArr;
         });
       }
     }
+  }
+  function hideFooter(){
+    setFooterVisible(false)   
   }
 
   return (
@@ -116,7 +137,11 @@ export default function App() {
             <h5 id="objective">Dont click the same character twice!</h5>
           </div>
         </div>
-        <Scores highScore = {highScore} currentScore = {currentScore} indexOfSelectedGame={indexOfSelectedGame}/>
+        <Scores
+          highScore={highScore}
+          currentScore={currentScore}
+          indexOfSelectedGame={indexOfSelectedGame}
+        />
       </div>
 
       <div
@@ -127,15 +152,16 @@ export default function App() {
           id="cardContainer"
           style={{ backgroundImage: `url(${backgroundImg})` }}
         >
-          {selectedGame.characters.map((character) => (
-            <CharacterCard
-              image={character.img}
-              onClickHandler={onClickCharacterHandler}
-              characterName={character.name}
-            />
+          {characterArr.map((character) => (             
+              <CharacterCard
+                image={character.img}
+                onClickHandler={onClickCharacterHandler}
+                characterName={character.name}
+              />
           ))}
         </div>
       </div>
+      {footerVisible===true?<footer onClick={hideFooter}><div>Pictures sorced from Disney.com</div><button onClick={hideFooter}>X</button></footer>:false}
     </div>
   );
 }
